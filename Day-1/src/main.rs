262e7fb11d5ea370bd40bb38ac0017b5b69bd4ad
NebelToast <juliussteude@gmail.com>
2025-12-01 20:00:52 +0100

    (Day-1) Complete part one

---
+++ Day-1/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+fn get_input() -> Vec<String> {
+    let input = fs::read_to_string("input.txt").expect("File couldn't be read");
+    let vec: Vec<String> = input.lines().map(|s| s.to_string()).collect();
+    vec
+}
+
+fn get_values(input: Vec<String>) -> Vec<i32> {
+    let mut values: Vec<i32> = Vec::new();
+    for elements in input {
+        let value = if elements.starts_with('L') {
+            -elements[1..].parse::<i32>().unwrap()
+        } else {
+            elements[1..].parse::<i32>().unwrap()
+        };
+        values.push(value);
+    }
+    values
+}
+
+fn get_zeros(values: Vec<i32>) -> u32 {
+    let mut dial = 50;
+    let mut zeros = 0;
+    for value in values {
+        dial = (dial + value) % 100;
+        if dial == 0 {
+            zeros += 1;
+        }
+    }
+    zeros
+}
+fn main() {
+    let input = get_input();
+    let values = get_values(input);
+    print!("{}", get_zeros(values));
+}
