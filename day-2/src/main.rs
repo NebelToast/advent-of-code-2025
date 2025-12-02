e4459bd664346addcc7124101bfbcea4fb6009d0
NebelToast <juliussteude@gmail.com>
2025-12-02 11:38:54 +0100

    (day-2) optimize solution of part one

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,15 +2,16 @@ use std::fs;
 
 fn get_input() -> Vec<(u64, u64)> {
     let input = fs::read_to_string("input.txt").expect("File couldn't be read");
-    let vec: Vec<String> = input.trim().split(",").map(|s| s.to_string()).collect();
-
-    vec.iter()
-        .map(|x| {
-            let val = x.split_once("-").unwrap();
-            (val.0.parse::<u64>().unwrap(), val.1.parse::<u64>().unwrap())
+    input
+        .trim()
+        .split(",")
+        .map(|s| {
+            let (start, end) = s.split_once("-").unwrap();
+            (start.parse::<u64>().unwrap(), end.parse::<u64>().unwrap())
         })
         .collect()
 }
+
 fn is_mirrorable(number: u64) -> bool {
     let number_str = number.to_string();
 
@@ -18,18 +19,17 @@ fn is_mirrorable(number: u64) -> bool {
     first_half == second_half
 }
 
-fn value_to_numbers(value: Vec<(u64, u64)>) -> Vec<u64> {
-    let mut numbers_vec: Vec<u64> = Vec::new();
+fn sum_of_invalid(value: Vec<(u64, u64)>) -> u64 {
+    let mut sum = 0;
     for (val1, val2) in value {
         for numbers in val1..=val2 {
             if is_mirrorable(numbers) {
-                dbg!(numbers);
-                numbers_vec.push(numbers);
+                sum += numbers
             }
         }
     }
-    numbers_vec
+    sum
 }
 fn main() {
-    println!("{}", value_to_numbers(get_input()).iter().sum::<u64>());
+    println!("{}", sum_of_invalid(get_input()));
 }
